652adddfe54bca667ff2c857691acb2561a704c6
yhiguchi <yhiguchi@uhuru.jp>
2018-07-02 03:34:12 +0900

    remove console.log()

--- simple-aws-sdk.js
+++ simple-aws-sdk.js
@@ -1,20 +1,14 @@
 module.exports = function(RED) {
   "use strict";
-
   function SimpleAWSSDK(n) {
     var AWS = require("aws-sdk");
     RED.nodes.createNode(this, n);
     var node = this;
-    
     node.on("input", function (msg) {
       try {
-        console.log(new Date().getTime());
         AWS.config.update(msg.aws.config);
-        console.log(new Date().getTime());
         var targetService = new AWS[msg.aws.service]();
-
         var callback = function (err, data) {
-          console.log(new Date().getTime());
           if (err) {
             node.status({ fill: "red", shape: "dot", text: "error" });
             node.error(err);
